dfba4937f4fbd9d2142af8c63478ca3b6cd8c576
HenriqueeLimaa <henrique.lima@pucrs.br>
2022-08-18 15:17:04 -0300

    Finish pages on javascript

--- src/index.js
+++ src/index.js
@@ -17,6 +17,9 @@ const createHeader = () => {
     const homeButton = document.createElement('button');
     homeButton.classList.add('nav-button', 'selected-nav-button');
     homeButton.textContent = 'Home';
+    // homeButton.addEventListener('click', () => {
+    //     createHomePage();
+    // })
     nav.appendChild(homeButton);
 
     const menu = document.createElement('button');
@@ -30,14 +33,13 @@ const createHeader = () => {
     nav.appendChild(contact);
 };
 
-
-const createHome = () => {
+const createHomePage = () => {
     const home = document.createElement('section');
     home.classList.add('home');
     content.appendChild(home);
 
     const homeBg = document.createElement('div');
-    homeBg.classList.add('home-bg');
+    homeBg.classList.add('body-bg');
     home.appendChild(homeBg);
 
     const textA = document.createElement('p');
@@ -61,5 +63,57 @@ const createHome = () => {
     homeBg.appendChild(textC);
 };
 
+const createMenuPage = () => {
+    const menu = document.createElement('section');
+    content.appendChild(menu);
+
+    const menuBg = document.createElement('div');
+    menuBg.classList.add('menu-bg');
+    menu.appendChild(menuBg);
+
+    for(let i=0; i<=5; i++){
+        const pizza1 = document.createElement('div');
+        pizza1.classList.add('food-container');
+        menuBg.appendChild(pizza1);
+
+        const pizzaIcon = document.createElement('img');
+        pizzaIcon.src = '../dist/images/pizza.png';
+        pizzaIcon.classList.add('food-img');
+        pizza1.appendChild(pizzaIcon);
+
+        const pizzaTitle = document.createElement('p');
+        pizzaTitle.classList.add('food-title');
+        pizzaTitle.textContent = 'Pizza'
+        pizza1.appendChild(pizzaTitle);
+
+        const pizzaIngredients = document.createElement('p');
+        pizzaIngredients.classList.add('food-ingredients');
+        pizzaIngredients.textContent = 'Tomato sauce, Mozarella, Tomato, Homemade sausage, Garlic, Basi';
+        pizza1.appendChild(pizzaIngredients);
+    }
+}
+
+const createContactPage = () => {
+    const contactPage = document.createElement('section');
+    contactPage.classList.add('contact-bg');
+    content.appendChild(contactPage);
+
+    const phoneNumber = document.createElement('p');
+    phoneNumber.classList.add('text');
+    phoneNumber.textContent = "Telefone: 4002-8922";
+    contactPage.appendChild(phoneNumber);
+
+    const adress = document.createElement('p');
+    adress.classList.add('text');
+    adress.textContent = 'Endereço: R. da Consolação, 3527 - Cerqueira César, São Paulo';
+    contactPage.appendChild(adress);
+
+    const restaurantMap = document.createElement('img');
+    restaurantMap.src = '../dist/images/restaurant-map.png';
+    restaurantMap.classList.add('restaurant-map');
+    contactPage.appendChild(restaurantMap);
+}
+
+
 createHeader();
-createHome();
+createContactPage();
